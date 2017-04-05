71dca229ebed52c146fa881e7cc2e125d408fe51
IljaDaderko <i.daderko@gmail.com>
2017-04-05 14:47:13 +0100

    Move injection to did mount

--- WebKitConfig/index.js
+++ WebKitConfig/index.js
@@ -2,12 +2,15 @@ import React, { PropTypes, Component } from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 
-injectTapEventPlugin();
-
 class WebKitConfig extends Component {
   // -- prop validation ----------------------------------------------------- //
   static propTypes = {
     children: PropTypes.oneOfType([PropTypes.node, PropTypes.array]).isRequired
+  }
+
+  // -- methods ------------------------------------------------------------- //
+  componentDidMount() {
+    injectTapEventPlugin();
   }
 
   render() {

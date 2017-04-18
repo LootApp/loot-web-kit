e2c52a5528ff454bc08298dbb79af71b7644bddc
IljaDaderko <i.daderko@gmail.com>
2017-04-18 13:30:13 +0300

    styled-components / prop-types packages, not building to /lib folder to decrease size

--- src/components/Test.js
+++ src/components/Test.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
+
+const SDiv = styled.div`
+  background-color: red;
+`;
 
 class TEST extends Component {
   render() {
-    return (<div>Hello World!</div>);
+    return (<SDiv>Hello World!</SDiv>);
   }
 }
 

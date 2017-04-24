b0868b3595b4aa492f6f9c5adbe168df0619b23c
IljaDaderko <idaderko@gmail.com>
2017-04-24 13:29:58 +0300

    Removed material icons dependency from date picker

--- src/components/InputDateTime.js
+++ src/components/InputDateTime.js
@@ -4,35 +4,9 @@ import { injectGlobal } from "styled-components";
 import MaterialDateTimePicker from "material-datetime-picker";
 import "material-datetime-picker/dist/material-datetime-picker.css";
 import { transitions, colours } from "../Constants";
+import arrow from "../assets/arrow-calendar.svg";
 
 injectGlobal`
-  @font-face {
-    font-family: 'Material Icons';
-    font-style: normal;
-    font-weight: 400;
-    src:
-      local('Material Icons'),
-      local('MaterialIcons-Regular'),
-      url(https://fonts.gstatic.com/s/materialicons/v22/2fcrYFNaTjcS6g4U3t-Y5UEw0lE80llgEseQY3FEmqw.woff2)
-      format('woff2');
-  }
-
-  .material-icons {
-    font-family: 'Material Icons';
-    font-weight: normal;
-    font-style: normal;
-    font-size: 24px;
-    line-height: 1;
-    letter-spacing: normal;
-    text-transform: none;
-    display: inline-block;
-    white-space: nowrap;
-    word-wrap: normal;
-    direction: ltr;
-    -webkit-font-feature-settings: 'liga';
-    -webkit-font-smoothing: antialiased;
-  }
-
   .c-btn--flat {
     padding: 7px 10px;
     border-radius: 3px;
@@ -42,6 +16,7 @@ injectGlobal`
 
     &:hover {
       background-color: ${colours.blue};
+      color: ${colours.white};
     }
   }
 
@@ -58,6 +33,29 @@ injectGlobal`
       background-color: ${colours.blue} !important;
     }
   }
+
+  .c-datepicker__day--selected {
+    font-weight: bold;
+  }
+
+  .c-datepicker__days {
+    margin-top: 0 !important;
+  }
+
+  .c-datepicker__back::before,
+  .c-datepicker__next::after {
+    content: '' !important;
+    width: 12px;
+    height: 12px;
+    display: inline-block;
+    background-image: url(${arrow});
+    background-size: cover;
+    opacity: 0.7;
+  }
+
+  .c-datepicker__back::before {
+    transform: rotate(180deg);
+  }
 `;
 
 class InputDateTime extends Component {
@@ -70,7 +68,7 @@ class InputDateTime extends Component {
       setTimeout(() => {
         this.overlay = document.querySelector(".c-scrim--shown");
         this.overlay.addEventListener("click", this._onCloseCalendar);
-      }, 500);
+      }, 100);
     })
     .on("close", () => {
       this.overlay.removeEventListener("click", this._onCloseCalendar);

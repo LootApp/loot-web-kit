ccbf8ba316e2876b3ed2ba71a19337f3537ba0ac
IljaDaderko <idaderko@gmail.com>
2017-04-25 11:03:53 +0300

    Don't import / show anything date picker related on mobile

--- src/components/InputDateTime.js
+++ src/components/InputDateTime.js
@@ -1,71 +1,75 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import styled, { injectGlobal } from "styled-components";
-// eslint-disable-next-line
-import MaterialDateTimePicker from "material-datetime-picker";
-import "material-datetime-picker/dist/material-datetime-picker.css";
 import { transitions, colours } from "../Constants";
-import arrow from "../assets/arrow-calendar.svg";
 import Input from "./Input";
+import isMobile from "../utilities/isMobile";
 
-// eslint-disable-next-line
-injectGlobal`
-  .c-btn--flat {
-    padding: 7px 10px;
-    border-radius: 3px;
-    text-transform: uppercase;
-    font-weight: semibold;
-    transition: ${transitions.base};
+let MaterialDateTimePicker;
+if (!isMobile()) {
+  MaterialDateTimePicker = require("material-datetime-picker");
+  require("material-datetime-picker/dist/material-datetime-picker.css");
+  const arrow = require("../assets/arrow-calendar.svg");
 
-    &:hover {
-      background-color: ${colours.blue};
-      color: ${colours.white};
+  // eslint-disable-next-line
+  injectGlobal`
+    .c-btn--flat {
+      padding: 7px 10px;
+      border-radius: 3px;
+      text-transform: uppercase;
+      font-weight: semibold;
+      transition: ${transitions.base};
+
+      &:hover {
+        background-color: ${colours.blue};
+        color: ${colours.white};
+      }
     }
-  }
 
-  .c-datepicker__header {
-    display: none;
-  }
-
-  .c-datepicker {
-    min-height: 380px !important;
-  }
-
-  .c-datepicker__day-body:hover {
-    &::before {
-      background-color: ${colours.blue} !important;
+    .c-datepicker__header {
+      display: none;
     }
-  }
 
-  .c-datepicker__days {
-    margin-top: 0 !important;
-  }
+    .c-datepicker {
+      min-height: 380px !important;
+    }
 
-  .c-datepicker__back::before,
-  .c-datepicker__next::after {
-    content: '' !important;
-    width: 12px;
-    height: 12px;
-    display: inline-block;
-    background-image: url(${arrow});
-    background-size: cover;
-    opacity: 0.7;
-  }
+    .c-datepicker__day-body:hover {
+      &::before {
+        background-color: ${colours.blue} !important;
+      }
+    }
 
-  .c-datepicker__back::before {
-    transform: rotate(180deg);
-  }
+    .c-datepicker__days {
+      margin-top: 0 !important;
+    }
 
-  .c-datepicker__toggle {
-    display: none !important;
-  }
+    .c-datepicker__back::before,
+    .c-datepicker__next::after {
+      content: '' !important;
+      width: 12px;
+      height: 12px;
+      display: inline-block;
+      background-image: url(${arrow});
+      background-size: cover;
+      opacity: 0.7;
+    }
 
-  .c-datepicker__day--disabled {
-    pointer-events: none;
-    opacity: 0.35;
-    cursor: not-allowed;
-  }
-`;
+    .c-datepicker__back::before {
+      transform: rotate(180deg);
+    }
+
+    .c-datepicker__toggle {
+      display: none !important;
+    }
+
+    .c-datepicker__day--disabled {
+      pointer-events: none;
+      opacity: 0.35;
+      cursor: not-allowed;
+    }
+  `;
+}
 
 const SInput = styled(Input)`
   appearance: none;
@@ -94,37 +98,44 @@ class InputDateTime extends Component {
     maxDate: null
   };
 
-  componentWillUnmount() {
-    this.overlay.removeEventListener("click", this._onCloseCalendar);
+  componentDidMount() {
+    if (!isMobile()) {
+      this.picker = new MaterialDateTimePicker({
+        default: this.props.defaultDate,
+        dateValidator: d => {
+          if (this.props.minDate && this.props.maxDate) {
+            return d >= this.props.minDate && d <= this.props.maxDate;
+          } else if (this.props.minDate) return d >= this.props.minDate;
+          else if (this.props.maxDate) return d <= this.props.maxDate;
+        }
+      })
+        .on("open", () => {
+          setTimeout(() => {
+            this.overlay = document.querySelector(".c-scrim--shown");
+            this.overlay.addEventListener("click", this._onCloseCalendar);
+          }, 100);
+        })
+        .on("close", () => {
+          this.overlay.removeEventListener("click", this._onCloseCalendar);
+        })
+        .on("submit", value => {
+          this.input.setState({
+            value: value.toDate().toISOString().substring(0, 10)
+          });
+        });
+
+      this._onOpenCalendar = () => this.picker.open();
+
+      this._onCloseCalendar = () => this.picker.close();
+    }
   }
 
-  _onOpenCalendar = () => this.picker.open();
-
-  _onCloseCalendar = () => this.picker.close();
-
-  picker = new MaterialDateTimePicker({
-    default: this.props.defaultDate,
-    dateValidator: d => {
-      if (this.props.minDate && this.props.maxDate) {
-        return d >= this.props.minDate && d <= this.props.maxDate;
-      } else if (this.props.minDate) return d >= this.props.minDate;
-      else if (this.props.maxDate) return d <= this.props.maxDate;
-    }
-  })
-    .on("open", () => {
-      setTimeout(() => {
-        this.overlay = document.querySelector(".c-scrim--shown");
-        this.overlay.addEventListener("click", this._onCloseCalendar);
-      }, 100);
-    })
-    .on("close", () => {
+  componentWillUnmount() {
+    if (!isMobile())
       this.overlay.removeEventListener("click", this._onCloseCalendar);
-    })
-    .on("submit", value => {
-      this.input.setState({
-        value: value.toDate().toISOString().substring(0, 10)
-      });
-    });
+  }
+
+  _onCalendarOpen = () => (!isMobile() ? this._onOpenCalendar() : null);
 
   _value = () => this.input._value();
 
@@ -140,7 +151,7 @@ class InputDateTime extends Component {
           this.input = input;
         }}
         readOnly
-        onFocus={this._onOpenCalendar}
+        onFocus={this._onCalendarOpen}
       />
     );
   }

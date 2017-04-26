11ba76cbfee93696791fcc9bebb526bfc00731d9
IljaDaderko <idaderko@gmail.com>
2017-04-26 12:50:57 +0300

    Added visa and master card graphics to InputCard

--- src/components/InputCard.js
+++ src/components/InputCard.js
@@ -1,6 +1,25 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
+import styled from "styled-components";
 import Input from "./Input";
+import visa from "../assets/visa-icon.svg";
+import mastercard from "../assets/mastercard-icon.svg";
+
+const SInut = styled(Input)`
+  position: relative;
+
+  &::after {
+    content: '';
+    display: block;
+    position: absolute;
+    right: 0;
+    top: 36px;
+    width: 30px;
+    height: 20px;
+    background-image: url(${props => props.cardIcon || "none"});
+    background-size: cover;
+  }
+`;
 
 class Card extends Component {
   static propTypes = {
@@ -13,12 +32,19 @@ class Card extends Component {
     minLength: 13
   };
 
+  state = {
+    cardIcon: null
+  };
+
   _onChange = value => {
-    let formatedValue = value.replace(/\s/g, "");
+    let formatedValue = value.toString().replace(/\s/g, "");
     formatedValue = formatedValue
       .replace(/[^\dA-Z]/g, "")
       .replace(/(.{4})/g, "$1 ")
       .trim();
+    if (formatedValue.match("^4")) this.setState({ cardIcon: visa });
+    else if (formatedValue.match("^5[1-5]"))
+      this.setState({ cardIcon: mastercard });
     return formatedValue;
   };
 
@@ -27,7 +53,8 @@ class Card extends Component {
   render() {
     const { maxLength, minLength, ...props } = this.props;
     return (
-      <Input
+      <SInut
+        cardIcon={this.state.cardIcon}
         type="tel"
         maxLength={maxLength}
         minLength={minLength}

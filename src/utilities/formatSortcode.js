cac7fec377c9bf1f40e64b94e649dfb43f6a20cf
Phillip Boateng <cnolimit@gmail.com>
2017-05-08 11:27:17 +0100

    updated the utility code

--- src/utilities/formatSortcode.js
+++ src/utilities/formatSortcode.js
@@ -2,23 +2,23 @@
  * @desc formats value in accordance to the options
  * @return {String}
  */
-const format = (options) => {
+const format = ({ occurance, delimiter, value }) => {
   // create a regex with the delimiter as a value to remove
-  const regex = new RegExp(`${options.delimiter}`, "g");
+  const regex = new RegExp(`${delimiter}`, "g");
 
   // sanatize the value by removing delimiter before use
-  const value = options.value.replace(regex, "");
+  const _value = value.replace(regex, "");
 
   // create new array to store the strings
   const inputAsArray = [];
 
-  for (let i = 0; i < value.length; i += 1) { // eslint-disable-next-line
+  for (let i = 0; i < _value.length; i += 1) { // eslint-disable-next-line
 
     // applies the delimiter at every point in which the options.occurance states
-    if (i !== 0 && i % options.occurance === 0) inputAsArray.push(options.delimiter);
+    if (i !== 0 && i % occurance === 0) inputAsArray.push(delimiter);
 
     // push the charecter into the array
-    inputAsArray.push(value[i]);
+    inputAsArray.push(_value[i]);
   }
 
   return inputAsArray.join().replace(/,/g, "");

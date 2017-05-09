a2cb379590facf1e4f1b3b7f4984fa05d80bbbb1
Phillip Boateng <cnolimit@gmail.com>
2017-05-09 10:44:01 +0100

    added examples and refactored the stringFormatter

--- src/utilities/formatSortcode.js
+++ src/utilities/formatSortcode.js
@@ -1,38 +1,13 @@
-/**
- * @desc formats value in accordance to the options
- * @return {String}
- */
-const format = ({ occurance, delimiter, value }) => {
-  // create a regex with the delimiter as a value to remove
-  const regex = new RegExp(`${delimiter}`, "g");
-
-  // sanatize the value by removing delimiter before use
-  const _value = value.replace(regex, "");
-
-  // create new array to store the strings
-  const inputAsArray = [];
-
-  for (let i = 0; i < _value.length; i += 1) { // eslint-disable-next-line
-
-    // applies the delimiter at every point in which the options.occurance states
-    if (i !== 0 && i % occurance === 0) inputAsArray.push(delimiter);
-
-    // push the charecter into the array
-    inputAsArray.push(_value[i]);
-  }
-
-  return inputAsArray.join().replace(/,/g, "");
-};
-
+import stringFormatter from "./stringFormatter";
 
 /**
  * @desc formats value into 'xx-xx-xx'
  * @return {String}
  */
 const formatSortcode = (value = "") =>
-  format({
+  stringFormatter({
     value: value.toString().replace(/[^0-9-]/g, ""),
-    delimiter: "-",
+    delimiter: " ",
     occurance: 2
   });
 

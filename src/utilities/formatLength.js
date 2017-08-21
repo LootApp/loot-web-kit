06b3b20c30404167c72fb3a8b734e2ff1cbd2284
Pedro Gomes <pedro@loot.io>
2017-08-21 12:37:40 +0100

    test new approach

--- src/utilities/formatLength.js
+++ src/utilities/formatLength.js
@@ -4,10 +4,17 @@
  * @param  {Number} occur
  * @return {Number}
  */
-const formatLength = (rchar, occur) => {
-  if (!occur) return rchar;
-  const length = (rchar - 1) / occur + rchar;
-  return Math.floor(length);
+// const formatLength = (rchar, occur) => {
+//   if (!occur) return rchar;
+//   const length = (rchar - 1) / occur + rchar;
+//   return Math.floor(length);
+// };
+
+const formatLength = (maxLength, occurance) => {
+  let requiredChar = 0;
+  if (maxLength % occurance === 0) requiredChar = maxLength - occurance;
+  else requiredChar = maxLength - maxLength % occurance;
+  return requiredChar;
 };
 
 export default formatLength;

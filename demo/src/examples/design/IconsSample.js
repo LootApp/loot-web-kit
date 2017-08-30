76e809ca6c0a90f3281d368673b011e9f2691da3
Pedro Gomes <pedro@loot.io>
2017-08-30 10:32:52 +0100

    remove duplicate icons and add nwb config url-loader

--- demo/src/examples/design/IconsSample.js
+++ demo/src/examples/design/IconsSample.js
@@ -1,7 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import Heading from "../../components/Heading";
-import Icons from "../../icons";
+import Icons from "../../../../src/icons";
 
 const Container = styled.div`
   display: flex;
--- nwb.config.js
+++ nwb.config.js
@@ -1,7 +1,33 @@
+let webpack = {};
+
+// -- webpack all config ---------------------------------------------------- //
+webpack = {
+  html: {
+    template: "docs/index.html"
+  }
+};
+
+// -- webpack production config --------------------------------------------- //
+if (process.env.NODE_ENV === "production") {
+  webpack = Object.assign({}, webpack, {
+    rules: {
+      graphics: false,
+      jpeg: false,
+      svg: false
+    },
+    extra: {
+      module: {
+        rules: []
+      }
+    }
+  });
+}
+
 module.exports = {
   type: "react-component",
   npm: {
     esModules: true,
     umd: false
-  }
+  },
+  webpack
 };

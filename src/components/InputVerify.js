467101aff1dada9c8a575013bcab38b936a9b0de
Phillip Boateng <cnolimit@gmail.com>
2017-09-13 17:29:56 +0100

    working on verify input

--- src/components/InputVerify.js
+++ src/components/InputVerify.js
@@ -17,8 +17,7 @@ const SInputVerify = styled.input`
   color: rgba(0, 0, 0, 0.6);
   background: rgb(255, 255, 255);
   transition: all 0.2s ease;
-  box-shadow: 0 10px 50px rgba(50, 50, 93, 0.12),
-    0 10px 20px rgba(50, 50, 93, 0.1);
+  box-shadow: 0 10px 50px rgba(50, 50, 93, 0.12), 0 10px 20px rgba(50, 50, 93, 0.1);
 
   &:focus {
     outline: none;
@@ -29,18 +28,25 @@ const SInputVerify = styled.input`
 class InputVerify extends Component {
   static propTypes = {
     fields: PropTypes.number,
-    onChange: PropTypes.func
+    onChange: PropTypes.func,
+    innerRef: PropTypes.func
   };
 
   static defaultProps = {
     fields: 4,
-    onChange: null
+    onChange: null,
+    innerRef: null
   };
 
   constructor() {
     super();
     this.verifyCode = [];
   }
+
+  state = {
+    inputs: [],
+    value: {}
+  };
 
   componentDidMount() {
     document.addEventListener("keydown", this.onKeyDown);
@@ -54,7 +60,7 @@ class InputVerify extends Component {
     if (event.which === 8) {
       const field = document.activeElement;
       if (field.value) {
-        field.value = "";
+        this.setState({ value: { ...this.state.value, [`value${field.id}`]: "" } });
         this.verifyCode[field.id] = field.value;
         this.props.onChange(this.verifyCode.join(""));
       } else {
@@ -66,14 +72,17 @@ class InputVerify extends Component {
   _onChange = ({ target }) => {
     const { value, id } = target;
     if (value) {
-      this[`input${id}`].value = value
-        .replace(/[^0-9]/g, "")
-        .substring(value.length - 1, value.length);
+      this.setState({
+        value: {
+          ...this.state.value,
+          [`value${id}`]: value.replace(/[^0-9]/g, "")
+        }
+      });
 
       if (id <= this.props.fields - 1 && !!value.replace(/[^0-9]/g, ""))
         this[`input${parseInt(id, 0) + 1}`].focus();
     }
-    this.verifyCode[id] = this[`input${id}`].value;
+    this.verifyCode[id] = this.state[`value${id}`];
     this.props.onChange(this.verifyCode.join(""));
   };
 
@@ -86,7 +95,9 @@ class InputVerify extends Component {
           type="tel"
           id={i}
           noValidate
+          maxLength={1}
           onChange={this._onChange}
+          value={this.state[`vale${i}`]}
           innerRef={input => (this[`input${i}`] = input)}
         />
       );
@@ -96,9 +107,12 @@ class InputVerify extends Component {
   };
 
   render() {
-    const { onChange, ...props } = this.props;
+    console.log(this);
+    const { onChange, innerRef, ...props } = this.props;
     return (
-      <SContainer {...props}>{this._createField(this.props.fields)}</SContainer>
+      <SContainer {...props}>
+        {this._createField(this.props.fields)}
+      </SContainer>
     );
   }
 }
